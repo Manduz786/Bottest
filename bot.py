77cb40b2a42f4299c2c328e572276d8aa45ee11d
Manduz786 <98330538+Manduz786@users.noreply.github.com>
2023-12-31 14:48:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,15 @@ api_id = "24928579"
 api_hash = "4e8991e8a9ae7aa37d6952d5b7ded2c5"
 bot_token = "6591255871:AAGFraAxEJyNDtA6rQTOQLd7CC83y-xM9zs"
 app = Client(
-        "my_bot",
-        api_id = api_id, api_hash = api_hash,
-        bot_token = bot_token
+        "my_bot",
+        api_id = api_id, api_hash = api_hash,
+        bot_token = bot_token
 )
-@app.on_message(filters.photo | filters.animation | filters.video | filters.docu
-ment)
+@app.on_message(filters.photo | filters.animation | filters.video | filters.document)
 async def fwd(bot,message):
-        try:
-                await asyncio.sleep(5)
-                await message.forward(-1002064526725)
-        except BadRequest as e:
-                print("error : " + str(e))
+        try:
+                await asyncio.sleep(5)
+                await message.forward(-1002064526725)
+        except BadRequest as e:
+                print("error : " + str(e))
 app.run()
