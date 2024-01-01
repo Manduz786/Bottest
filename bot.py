74de5942789bb67ddc40aff2746a24667b934878
Leos <98330538+Manduz786@users.noreply.github.com>
2024-01-01 18:06:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ app = Client("my_bot",api_id = api_id, api_hash = api_hash,bot_token = bot_token
 async def fwd(bot,message):
     try:
         #await asyncio.sleep(5)
-        await message.forward(-1002064526725)
+        await message.forward(-1002085625613)
     except BadRequest as e:
        print("error : " + str(e))
 app.run()
