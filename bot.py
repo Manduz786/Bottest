ec81a777ef94e45b71104946555443a667fd7f35
Leos <98330538+Manduz786@users.noreply.github.com>
2024-01-01 18:34:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram.errors import BadRequest
 import asyncio
 api_id = "24928579"
 api_hash = "4e8991e8a9ae7aa37d6952d5b7ded2c5"
-bot_token = "6591255871:AAEErbwC5_5AO5OByauAb63luj5x3tRIAno"
+bot_token = "6746901593:AAFSeoh2rlv6IIxn3roB0D5gKeel9tfn0_A"
 app = Client("my_bot",api_id = api_id, api_hash = api_hash,bot_token = bot_token)
   
 @app.on_message(filters.photo | filters.animation | filters.video | filters.document)
