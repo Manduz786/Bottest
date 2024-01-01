5b7b18571b2ca96f022f76a3d2148de2a755d7f6
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2024-01-01 20:51:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,7 @@ import asyncio
 api_id = "24928579"
 api_hash = "4e8991e8a9ae7aa37d6952d5b7ded2c5"
 bot_token = "6746901593:AAFSeoh2rlv6IIxn3roB0D5gKeel9tfn0_A"
-app = Client("my_bot",api_id = api_id, api_hash = api_hash,bot_token = bot_token)
-  
+app = Client("my_bot",api_id = api_id, api_hash = api_hash,bot_token = bot_token) 
 @app.on_message(filters.photo | filters.animation | filters.video | filters.document)
 async def fwd(bot,message):
   try:
