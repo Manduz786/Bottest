18354605f0904f034353a90ee66dbc99bde65e81
Leos <98330538+Manduz786@users.noreply.github.com>
2024-01-01 20:31:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,17 @@ import asyncio
 api_id = "24928579"
 api_hash = "4e8991e8a9ae7aa37d6952d5b7ded2c5"
 bot_token = "6746901593:AAFSeoh2rlv6IIxn3roB0D5gKeel9tfn0_A"
-app = Client("my_bot",api_id = api_id, api_hash = api_hash,bot_token = bot_token)
-  
+app = Client("my_bot",
+        api_id = api_id, api_hash = api_hash,
+        bot_token = bot_token
+)
+
+GROUP_ID = -1002085625613
 @app.on_message(filters.photo | filters.animation | filters.video | filters.document)
 async def fwd(bot,message):
-  try:
-    await asyncio.sleep(5)
-    await message.forward(-1002085625613)
-  except BadRequest as e:
-    print("error : " + str(e))
+        try:
+                await asyncio.sleep(5)
+                await message.forward(-1002085625613)
+        except BadRequest as e:
+                print("error : " + str(e))
 app.run()
